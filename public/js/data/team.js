0f56aa59a92616b7f5df02914b70366cd5b7484f
andreudotai <ubongudotai@gmail.com>
2026-06-17 11:56:25 +0100

    feat: converted the individual team member pages to EJS templates and added new team members to the data file
    - Updated the team member pages to use EJS templates for dynamic content rendering.

---
+++ public/js/data/team.js
@@ -0,0 +1,168 @@
+const teamMembers = [
+  {
+    id: 1,
+    slug: "basil-udotai",
+    name: "Basil Udotai",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/blog/blog-03.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Basil Udotai is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "basil@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://www.linkedin.com/in/basiludotai/"
+  },
+    {
+    id: 2,
+    slug: "rotimi-ogunyemi",
+    name: "Rotimi Ogunyemi",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/rotimi-ogunyemi.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Rotimi Ogunyemi is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "rotimi@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/rotimiogunyemi"
+  },
+    {
+    id: 3,
+    slug: "senator-ihenyen",
+    name: "Senator Ihenyen",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/senator-ihenyen.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Senator Ihenyen is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "senator@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/senatorihenyen"
+  },
+    {
+    id: 4,
+    slug: "joseph-nwobike",
+    name: "Dr. Joseph Nwobike, SAN",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/joseph-nwobike.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Dr. Joseph Nwobike is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "joseph@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/josephnwobike"
+  },
+    {
+    id: 5,
+    slug: "edidiong-idiong",
+    name: "Edidiong Idiong",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/edidiong-idiong.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Edidiong Idiong is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "edidiong@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/edidiongidiong"
+  },
+    {
+    id: 6,
+    slug: "muhaimin-abdulmalik",
+    name: "Muhaimin Abdulmalik",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/muhaimin-abdulmalik.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Muhaimin Abdulmalik is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "mulik@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/muhaiminabdulmalik"
+  },
+    {
+    id: 7,
+    slug: "fortune-ugwumba",
+    name: "Fortune Ugwumba",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/fortune-ugwumba.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Fortune Ugwumba is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "fortune@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/fortuneugwumba"
+  },
+    {
+    id: 8,
+    slug: "favour-nwadibie",
+    name: "Favour Nwadibie",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/favour-nwadibie.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Favour Nwadibie is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "favour@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/favournwadibie"
+  },
+    {
+    id: 9,
+    slug: "Lauretta-umeh",
+    name: "Lauretta Umeh",
+    role: "Lead Consultant & Founding Partner",
+    image: "/images/team/lauretta-umeh.jpg",
+    introduction: "Nigeria’s “Father of Technology Law”",
+    bio: "Lauretta Umeh is the Lead Consultant at Johnson & Wilner LLP. He served as the Director of Cybersecurity at the Office of the National Security Adviser...",
+    experiences: [
+      "Former Director of Cybersecurity, Office of the National Security Adviser",
+      "Former General Counsel, National Information Technology Development Agency (NITDA)",
+      "Pioneer Head of the Directorate for Cybersecurity"
+    ],
+    expertise: ["Technology Policy", "Cybersecurity", "Data Governance", "Digital Regulation"],
+    email: "lauretta@johnsonandwilner.com",
+    phone: "+234 816 005 1310",
+    linkedin: "https://linkedin.com/in/laurettatumeh"
+  },
+
+  // Add more members here...
+];
+
+module.exports = teamMembers;   // For CommonJS compatibility
