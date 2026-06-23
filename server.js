0a6fafd0727d523f4298f5bd49241d4d352ed9a9
Joseph-001-V <josephsadiku2002@gmail.com>
2026-06-23 15:35:23 +0100

    Highlight the active navigation menu item based on the current page route.

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const mainRoutes = require('./routes/mainRoutes');
+// Make the current URL path available to all views.
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path;
+  next();
+});
 app.use('/', mainRoutes);
 
 // 404
